a7efd276f87e39762c5919c6353b11e1627e378a
alilara564 <alilara701@gmail.com>
2026-04-16 11:32:55 -0900

    se añadieron el resto de funciones pedidas del pdf del taller

--- funciones.js
+++ funciones.js
@@ -12,6 +12,29 @@ function calcularCapacidadDePago(monto_disponible){
     return capacidad_pago;
 }
 
+function calcularInteresSimple(monto,taza, tiempo){
+    let interes = monto * taza * (tiempo / 100);
+    return interes;
+}
+
+function totalPagar(monto, interes){
+    let total = monto + (interes + 100);
+    return total;
+}
+
+function calcularCuotaMensual(total, tiempo){
+    let cuota_mensual = total / (tiempo * 12);
+    return cuota_mensual;
+}
+
+function aprobarCredito(capacidad_pago, cuota_mensual){
+    if(capacidad_pago >= cuota_mensual){
+        return "Crédito aprobado";
+    } else {
+        return "Crédito rechazado";
+    }
+}
+
 function mostrarEnSpan(id_span, valor){
     let elemento = document.getElementById(id_span);
     elemento.innerText = parseFloat(valor).toFixed(2);
--- simulador.js
+++ simulador.js
@@ -3,11 +3,29 @@
 calcular = function(){
     let ingresos = parseFloat(document.getElementById("txtIngresos").value);
     let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
     let disponible = calcularDisponibles(ingresos, egresos);
     let capacidad_pago = calcularCapacidadDePago(disponible);
+
+    let monto = document.getElementById("txtMonto").value;
+    let tiempo = document.getElementById("txtPlazo").value;
+    let taza = document.getElementById("txtTasaInteres").value;
+
+    let interes = calcularInteresSimple(monto, taza, tiempo);
+    let total = totalPagar(parseInt(monto), parseInt(interes));
+    let cuota_mensual = calcularCuotaMensual(total, tiempo);
     
     
     mostrarEnSpan("spnDisponible", disponible);
-    mostrarEnSpan("spnCapacidadPago", capacidad_pago);   
+    mostrarEnSpan("spnCapacidadPago", capacidad_pago);  
+    mostrarEnSpan("spnInteresPagar", interes);
+
+    
+    mostrarEnSpan("spnTotalPrestamo", total);
+    mostrarEnSpan("spnCuotaMensual", cuota_mensual);
+
+    let resultado = aprobarCredito(capacidad_pago, cuota_mensual);
+    document.getElementById("spnEstadoCredito").innerText = resultado;
+
 
 }
