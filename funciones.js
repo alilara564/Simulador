605c37802441d2ebddfc235bcd574e982c113c9d
alilara564 <alilara701@gmail.com>
2026-04-15 20:49:30 -0900

    se crearon las funciones encomendadas

--- funciones.js
+++ funciones.js
@@ -1,1 +1,8 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponibles(ingresos,egresos){
+    let disponibles = ingresos - egresos;
+    if(disponibles < 0){
+        disponibles = 0;
+    }
+    return disponibles;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,14 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+import { calcularDisponibles } from "./funciones.js";
+
+
+function calcular(){
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    let disponible = calcularDisponibles(ingresos, egresos);
+    
+    
+    const elemento = document.getElementById('spnDisponible');
+    elemento.innerText =  disponible.toFixed(2);
+}
